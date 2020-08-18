e533cc673c7d258ec329a7b6dbe5fb4996a2e967
Fabio <andresviveros1984@googlemail.com>
2020-08-18 14:46:49 +0100

    added edit features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,40 @@ class ArticlesController < ApplicationController
     end
 
     def index
-        @articles = Article.all
+        # binding.pry
+      @articles = Article.all
     end
+
+    def new
+      @article = Article.new      
+    end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def create
+        
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      if(@article.save)
+        flash[:notice] ="Article saved successfully" #=>to print message if save successfully
+        redirect_to article_path(@article)
+        # redirect_to @article => alternative sytanx to above
+      else
+        render 'new'
+      end
+    end
+
+   def update
+      @article = Article.find(params[:id]) 
+      if(@article.update(params.require(:article).permit(:title, :description)))
+        flash.notice ="Update of article successfull"
+        redirect_to @article
+      else
+        render 'edit'
+      end
+   end
+
+   
 
 end

8ed37e37d01f50d164409ca1c4393a9e0972879f
Fabio <andresviveros1984@googlemail.com>
2020-10-09 15:20:15 +0100

    added pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
 
     def index
         # binding.pry to debug
-      @articles = Article.all
+      @articles = Article.paginate(page: params[:page], per_page: 5)
     end
 
     def new
